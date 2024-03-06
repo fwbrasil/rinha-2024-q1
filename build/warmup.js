90c576a025901bbd726d640e911d12a8394b865b
Flavio Brasil <fwbrasil@gmail.com>
2024-03-06 13:42:12 -0800

    .

--- build/warmup.js
+++ build/warmup.js
@@ -4,14 +4,16 @@ import { sleep, check } from 'k6';
 export let options = {
     stages: [
         { duration: '1s', target: 4 },
-        { duration: '40s', target: 4 },
+        { duration: '10s', target: 4 },
     ]
 };
 
 export default function () {
 
-    const transactionUrl = 'http://localhost:9999/clientes/0/transacoes';
-    const extratoUrl = 'http://localhost:9999/clientes/0/extrato';
+    const transactionUrl1 = 'http://localhost:8081/clientes/0/transacoes';
+    const extratoUrl1 = 'http://localhost:8081/clientes/0/extrato';
+    const transactionUrl2 = 'http://localhost:8082/clientes/0/transacoes';
+    const extratoUrl2 = 'http://localhost:8082/clientes/0/extrato';
     const headers = { 'Content-Type': 'application/json' };
 
     // Alternating transaction type for each iteration
@@ -29,10 +31,14 @@ export default function () {
     };
 
     // Perform a transaction with a timeout
-    let transactionRes = http.post(transactionUrl, payload, requestOptions);
-    check(transactionRes, { 'transaction status was 200': (r) => r.status == 200 });
+    let transactionRes1 = http.post(transactionUrl1, payload, requestOptions);
+    check(transactionRes1, { 'transaction status was 200': (r) => r.status == 200 });
+    let transactionRes2 = http.post(transactionUrl2, payload, requestOptions);
+    check(transactionRes2, { 'transaction status was 200': (r) => r.status == 200 });
 
     // Retrieve the account statement with a timeout
-    let extratoRes = http.get(extratoUrl, { timeout: '1s' }); // Apply timeout to GET request
-    check(extratoRes, { 'extrato status was 200': (r) => r.status == 200 });
+    let extratoRes1 = http.get(extratoUrl1, { timeout: '1s' }); // Apply timeout to GET request
+    check(extratoRes1, { 'extrato status was 200': (r) => r.status == 200 });
+    let extratoRes2 = http.get(extratoUrl2, { timeout: '1s' }); // Apply timeout to GET request
+    check(extratoRes2, { 'extrato status was 200': (r) => r.status == 200 });
 }
